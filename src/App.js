7cc293613820fe3910bdad30e72b10ee9f069e85
Eugenia Grima <eugeniagrima@gmail.com>
2022-07-25 16:51:07 -0300

    tp

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import logo from './images/LogoReact.png';
 import './App.css';
 import NavBar from './NavBar';
 
+const styles = { margin: "20px", color: "black", textAlign: "center", size: "2rem"}
+
 function App() {
   return (
     <div className="App">
+      <NavBar>
+      style= {styles} //ESTO VA ACA??
+        </NavBar> 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
